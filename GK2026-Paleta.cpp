010b35d2fee5e1c3da7636d734133027d27473b0
Kosmmic <michalekkosciolek@gmail.com>
2026-04-26 14:26:07 +0200

    Praca nad projektem- laboratorium projektowe nr2

---
+++ GK2026-Paleta.cpp
@@ -0,0 +1,4 @@
+// funkcje do redukcji kolorów i tworzenia palet
+#include "GK2026-Paleta.h"
+#include "GK2026-Zmienne.h"
+#include "GK2026-Funkcje.h"
---
+++ GK2026-Pliki.cpp
@@ -0,0 +1,6 @@
+// funkcje do operacji na plikach
+#include "GK2026-Funkcje.h"
+#include "GK2026-Zmienne.h"
+#include "GK2026-Paleta.h"
+#include "GK2026-MedianCut.h"
+#include "GK2026-Pliki.h"
